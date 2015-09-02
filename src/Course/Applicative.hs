89d5a1e26c45c28ccba86333aadd645e75e73b19
Vivitsu Maharaja <vivitsu.maharaja@gmail.com>
2015-09-02 15:57:50 -0700

    Solve Applicative exercises

--- src/Course/Applicative.hs
+++ src/Course/Applicative.hs
@@ -46,8 +46,8 @@ class Apply f => Applicative f where
   (a -> b)
   -> f a
   -> f b
-(<$>) =
-  error "todo: Course.Applicative#(<$>)"
+(<$>) f a =
+  pure f <*> a
 
 -- | Insert into Id.
 --
@@ -57,7 +57,7 @@ instance Applicative Id where
     a
     -> Id a
   pure =
-    error "todo: Course.Applicative pure#instance Id"
+    Id
 
 -- | Insert into a List.
 --
@@ -67,7 +67,7 @@ instance Applicative List where
     a
     -> List a
   pure =
-    error "todo: Course.Applicative pure#instance List"
+    (:. Nil)
 
 -- | Insert into an Optional.
 --
@@ -77,7 +77,7 @@ instance Applicative Optional where
     a
     -> Optional a
   pure =
-    error "todo: Course.Applicative pure#instance Optional"
+    Full
 
 -- | Insert into a constant function.
 --
@@ -87,7 +87,7 @@ instance Applicative ((->) t) where
     a
     -> ((->) t a)
   pure =
-    error "todo: Course.Applicative pure#((->) t)"
+    const
 
 -- | Sequences a list of structures to a structure of list.
 --
@@ -110,7 +110,7 @@ sequence ::
   List (f a)
   -> f (List a)
 sequence =
-  error "todo: Course.Applicative#sequence"
+  foldRight (lift2 (:.)) (pure Nil)
 
 -- | Replicate an effect a given number of times.
 --
@@ -133,8 +133,8 @@ replicateA ::
   Int
   -> f a
   -> f (List a)
-replicateA =
-  error "todo: Course.Applicative#replicateA"
+replicateA n =
+  sequence . replicate n
 
 -- | Filter a list with a predicate that produces an effect.
 --
@@ -161,8 +161,10 @@ filtering ::
   (a -> f Bool)
   -> List a
   -> f (List a)
-filtering =
-  error "todo: Course.Applicative#filtering"
+filtering p =
+  foldRight f (pure Nil)
+      -- This makes my brain hurt
+      where f x = lift2 (\b -> if b then (x :.) else id) (p x)
 
 -----------------------
 -- SUPPORT LIBRARIES --
